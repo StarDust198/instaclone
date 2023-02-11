536ec69e3fa30ce37c8089ee38a4c8ecb23ae730
Sergei Zhilinskij <roadtomars2030@gmail.com>
2023-02-11 14:38:47 +0400

    header sticky & shadow

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,46 +10,48 @@ type Props = {};
 
 export const Header = (props: Props) => {
   return (
-    <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
-      {/* Left */}
-      <div className="w-24 h-24 relative hidden lg:inline-grid cursor-pointer">
-        <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png"
-          alt="Instagram logo"
-          fill
-          className="object-contain"
-        />
-      </div>
-      <div className="w-10 h-24 relative lg:hidden cursor-pointer">
-        <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/132px-Instagram_logo_2016.svg.png"
-          alt="Instagram logo"
-          fill
-          className="object-contain"
-        />
-      </div>
-
-      {/* Middle */}
-      <div className="relative mt-1">
-        <div className="absolute top-2 left-2">
-          <MagnifyingGlassIcon className="h-5 text-gray-500" />
+    <div className="shadow-sm border-b sticky top-0 bg-white z-10">
+      <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
+        {/* Left */}
+        <div className="w-24 h-24 relative hidden lg:inline-grid cursor-pointer">
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png"
+            alt="Instagram logo"
+            fill
+            className="object-contain"
+          />
         </div>
-        <input
-          type="text"
-          placeholder="Search"
-          className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
-        />
-      </div>
+        <div className="w-10 h-24 relative lg:hidden cursor-pointer">
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/132px-Instagram_logo_2016.svg.png"
+            alt="Instagram logo"
+            fill
+            className="object-contain"
+          />
+        </div>
 
-      {/* Right */}
-      <div className="flex items-center space-x-4">
-        <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
-        <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/800px-User_icon_2.svg.png"
-          alt="User image"
-          className="h-10 rounded-full"
-        />
+        {/* Middle */}
+        <div className="relative mt-1">
+          <div className="absolute top-2 left-2">
+            <MagnifyingGlassIcon className="h-5 text-gray-500" />
+          </div>
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+          />
+        </div>
+
+        {/* Right */}
+        <div className="flex items-center space-x-4">
+          <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/800px-User_icon_2.svg.png"
+            alt="User image"
+            className="h-10 rounded-full"
+          />
+        </div>
       </div>
     </div>
   );
