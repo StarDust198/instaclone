0f1a07faa2572943642f4dfe9084751b966ae824
Sergei Zhilinskij <roadtomars2030@gmail.com>
2023-02-12 00:22:55 +0400

    add post component

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,10 +1,30 @@
+import { EllipsisHorizontalIcon } from '@heroicons/react/24/outline';
+
 import { IPost } from '@/interfaces/post.interface';
-import { DetailedHTMLProps, HTMLAttributes } from 'react';
 
-interface PostProps
-  extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>,
-    Omit<IPost, 'id'> {}
+export const Post = ({
+  img,
+  userImg,
+  username,
+  id,
+  caption,
+  ...props
+}: IPost) => {
+  return (
+    <div className="bg-white my-7 border rounded-md">
+      {/* Post Header */}
+      <div className="flex items-center p-5">
+        <img
+          src={userImg}
+          alt={username}
+          className="h-12 rounded-full object-cover border p-1 mr-3"
+        />
+        <p className="font-bold flex-1">{username}</p>
+        <EllipsisHorizontalIcon className="h-5" />
+      </div>
 
-export const Post = (props: PostProps) => {
-  return <div {...props}>Post</div>;
+      {/* Post Image */}
+      <img className="object-cover w-full" src={img} alt={caption} />
+    </div>
+  );
 };
