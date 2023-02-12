ef37e6f7aa44fda0e12f9a31f5594d4053234183
Sergei Zhilinskij <roadtomars2030@gmail.com>
2023-02-12 19:35:44 +0400

    responsive sections

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -4,15 +4,15 @@ import { Stories } from './Stories';
 type Props = {};
 export const Feed = (props: Props) => {
   return (
-    <main>
-      <section>
+    <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto">
+      <section className="md:col-span-2">
         {/* Stories */}
         <Stories />
 
         {/* Posts */}
         <Posts />
       </section>
-      <section>
+      <section className="hidden md:inline-grid md:cols-span-1">
         {/* Mini profile */}
 
         {/* Suggestions */}
